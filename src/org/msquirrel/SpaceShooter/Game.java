380ef794fd934f3b98026c514adb2ddfdf5c590a
Tom Todd <thomas.m.a.todd@gmail.com>
2013-02-24 21:37:33 +0000

    Changed BuildPath to include Slick2D and lwjgl. Implemented Slick2D Methods in to Game class.

--- src/org/msquirrel/SpaceShooter/Game.java
+++ src/org/msquirrel/SpaceShooter/Game.java
@@ -1,5 +1,29 @@
 package org.msquirrel.SpaceShooter;
 
-public class Game{
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Game extends BasicGame{
+
+	public Game(String title) {
+		super(title);
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g) throws SlickException {
+
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+
+	}
+
+	@Override
+	public void update(GameContainer container, int delta) throws SlickException {
+
+	}
 
 }
