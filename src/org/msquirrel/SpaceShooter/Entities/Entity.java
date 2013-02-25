251b685152b8d0ca2177bcb2ce413b9210d654f8
Tom Todd <thomas.m.a.todd@gmail.com>
2013-02-25 23:11:20 +0000

    Added hitBox rectangle to the Entity class. Added a getter and setter for hitbox.

--- src/org/msquirrel/SpaceShooter/Entities/Entity.java
+++ src/org/msquirrel/SpaceShooter/Entities/Entity.java
@@ -2,15 +2,29 @@ package org.msquirrel.SpaceShooter.Entities;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
 
 public class Entity {
 	private float x;
 	private float y;
 	private boolean alive;
+	private Image entityImage;
+	private Rectangle hitBox;
 	
 	public Entity(float x, float y){
 		this.x = x;
 		this.y = y;
+		this.hitBox = new Rectangle(this.x, this.y, entityImage.getWidth(), entityImage.getHeight());
+	}
+	
+	public void setHitBox(float x, float y, float width, float height){
+		this.hitBox = new Rectangle(x,y,width,height);
+	}
+	
+	public Rectangle getHitBox(){
+		return hitBox;
 	}
 	
 	public void update(GameContainer container, int delta){
